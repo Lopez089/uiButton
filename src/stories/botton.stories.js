ca1ae8f7f4a15c3759fd482d8784e596d8330e38
Juan Lopez <aragonlopezjuan87@gmail.com>
2022-02-07 22:50:04 +0100

    feat : first commit

---
+++ src/stories/botton.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from '../elements/button'
+
+export default {
+  title: 'Button',
+  component: Button
+}
+
+export const Primary = () => <Button>Hola</Button>
